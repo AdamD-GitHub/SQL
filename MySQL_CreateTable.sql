f77fd9e6ea5166becfa8ff3f89acb0fab31ad3fe
AdamD-GitHub <49349488+AdamD-GitHub@users.noreply.github.com>
2019-04-17 17:03:44 -0500

    Add files via upload

---
+++ MySQL_CreateTable.sql
@@ -0,0 +1,9 @@
+$TableName = "visitors"; 
+
+$SQLstring = "CREATE TABLE $TableName
+			(countID SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+			 last_name VARCHAR(40), 
+			 first_name VARCHAR(40))"; 
+
+
+	  
