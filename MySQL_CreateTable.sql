4a4f428d69ab2996fa3f84f7c4b5cf2da079f74b
AdamD-GitHub <49349488+AdamD-GitHub@users.noreply.github.com>
2019-04-17 17:22:04 -0500

    Update MySQL_CreateTable.sql

--- MySQL_CreateTable.sql
+++ MySQL_CreateTable.sql
@@ -1,5 +1,9 @@
-$TableName = "visitors"; 
+/* Create Database */
+$DBName = "guestbook"; 
+$SQLstring = "CREATE DATABASE $DBName";
 
+/* Create table */
+$TableName = "visitors"; 
 $SQLstring = "CREATE TABLE $TableName
 	     (countID SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 	      last_name VARCHAR(40), 
