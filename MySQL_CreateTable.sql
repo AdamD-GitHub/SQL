4600ad6b38639178bd9f05d9ad3f5728a19104f5
AdamD-GitHub <49349488+AdamD-GitHub@users.noreply.github.com>
2019-04-17 17:09:45 -0500

    Update MySQL_CreateTable.sql

--- MySQL_CreateTable.sql
+++ MySQL_CreateTable.sql
@@ -1,9 +1,9 @@
 $TableName = "visitors"; 
 
 $SQLstring = "CREATE TABLE $TableName
-			(countID SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-			 last_name VARCHAR(40), 
-			 first_name VARCHAR(40))"; 
+	     (countID SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	      last_name VARCHAR(40), 
+	      first_name VARCHAR(40))"; 
 
 
 	  
